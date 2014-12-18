23dc14c0b85a569470025cf8d942fc16e1f25b28
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2014-12-18 13:49:18 -0500

    Reading from the potentiometer
    Lessons
    - Be careful, the potentiometer dial is small. Take care not to slip and
    damage your components.
    - Think about how you organise your wiring to make accessing the pot
    (potentiometer) more easily

--- bot.js
+++ bot.js
@@ -4,4 +4,9 @@ var board = new five.Board();
 board.on("ready", function () {
     var led = new five.Led(11);
     led.pulse(1000);
+
+    var input = new five.Sensor("A0");
+    input.on("data", function() {
+        console.log(this.value);
+    });
 });
