def596d1877ee9517dcc6117ab81a2b9f1531761
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2014-12-18 14:14:31 -0500

    Controlling LED with potentiometer
    Lessons
    - Consider debouncing potentiometer value (get mode value of several
    readings to debounce small flickers)

--- bot.js
+++ bot.js
@@ -2,11 +2,23 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function () {
+    var initialValue;
     var led = new five.Led(11);
-    led.pulse(1000);
-
     var input = new five.Sensor("A0");
-    input.on("data", function() {
-        console.log(this.value);
+    
+    var setLed = function (value) {
+        // sway +/- points to control the LED
+        var sway = 10;
+        var brightness = five.Fn.map(value, initialValue - sway, initialValue + sway, 0, 255);
+        
+        console.log("setting LED to " + brightness);
+        led.brightness(brightness);
+    };
+    
+    input.on("data", function () {
+        if (initialValue === undefined) {
+            initialValue = this.value;
+        }
+        setLed(this.value);
     });
 });
